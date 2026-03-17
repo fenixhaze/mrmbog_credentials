4af6026d0d8d71eb00659c3903bce61438a3bf06
fenixhaze <cami_vaca93@hotmail.com>
2026-03-17 18:14:00 +0000

    Commit request 17 mar 1:13

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const authConfig = {
     auth: {
         clientId: "23d1168d-113b-48c0-a4fe-6e6d743f77af",
         authority: "https://login.microsoftonline.com/d026e4c1-5892-497a-b9da-ee493c9f0364",
-        redirectUri: "https://fenixhaze.github.io/mrmbog_credentials/", 
+        redirectUri: "https://zealous-sky-0305e650f.2.azurestaticapps.net", 
     },
     cache: { cacheLocation: "sessionStorage", storeAuthStateInCookie: false }
 };
@@ -387,4 +387,4 @@ export default function App() {
         <UnauthenticatedTemplate><LoginScreen /></UnauthenticatedTemplate>
     </MsalProvider>
   ); 
-}
+}//force end
