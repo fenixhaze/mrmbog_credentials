ea8c3ad13e6dfc6593d0e4ae369cc73257335323
CamiloVacaMRM <camilo.vaca@mrm.com>
2026-04-17 12:16:49 -0500

    Update Azure auth redirect URI to GitHub Pages URL

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const authConfig = {
   auth: {
     clientId: "23d1168d-113b-48c0-a4fe-6e6d743f77af",
     authority: "https://login.microsoftonline.com/d026e4c1-5892-497a-b9da-ee493c9f0364",
-    redirectUri: "https://zealous-sky-0305e650f.2.azurestaticapps.net",
+    redirectUri: "https://fenixhaze.github.io/mrmbog_credentials/",
   },
   cache: { cacheLocation: "sessionStorage", storeAuthStateInCookie: false }
 };
